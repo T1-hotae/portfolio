0c6fb762c4a9293e87fcc7e3bca95a695d2e0fcb
HwangHotae <hotae0321@naver.com>
2026-09-15 16:29:08 +0900

    fix : Pofol 링크 추가

--- src/data/projects/pofol.js
+++ src/data/projects/pofol.js
@@ -57,7 +57,7 @@ export const pofol = {
     "OpenAI",
   ],
   thumbnail: new URL("../../assets/icons/Pofol.png", import.meta.url).href,
-  github: null,
+  github: "https://github.com/PoFol-Capstone/Frontend/commits/main/",
   demo: "https://pofol-community.vercel.app/",
   youtube: "https://youtu.be/bewrnvM06zs",
   period: "2026.03 ~ 2026.06",
